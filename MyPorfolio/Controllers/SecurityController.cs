589d11dc693505f664016ce6b82dee9b68cb2193
phamquangviet891 <82923647+VietPam@users.noreply.github.com>
2024-06-20 10:19:08 +0700

    feat: Login page setup

---
+++ MyPorfolio/Controllers/SecurityController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyPorfolio.Models.Context;
+using MyPorfolio.Models.Entities;
+using System.Security.Claims;
+
+namespace MyPorfolio.Controllers;
+
+[AllowAnonymous]
+public class SecurityController : Controller
+{
+    MyPorfolioContext db = new MyPorfolioContext();
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult login(Admin admin)
+    {
+        Admin? data = db.Admins.FirstOrDefault(x => x.UserName == admin.UserName && x.Password == admin.Password);
+        if (data != null)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,admin.UserName)
+            };
+            ClaimsIdentity userIdentity = new ClaimsIdentity(claims, "Security");
+            ClaimsPrincipal principle = new ClaimsPrincipal(userIdentity);
+            HttpContext.SignInAsync(principle);
+
+            return RedirectToAction("Index", "Admin");
+        }
+
+        return View();
+    }
+
+    public IActionResult Logout()
+    {
+        HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return RedirectToAction("Login");
+    }
+}
--- MyPorfolio/Program.cs
+++ MyPorfolio/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using MyPorfolio.Models.Context;
+
 namespace MyPorfolio;
 
 public class Program
@@ -5,6 +11,22 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddDbContext<MyPorfolioContext>(
+            s => s.UseSqlite(builder.Configuration.GetConnectionString("default")));
+
+        builder.Services.AddAuthentication
+            (CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(c =>
+                {
+                    c.LoginPath = "/Security/Login";
+                });
+
+        builder.Services.AddMvc(config =>
+        {
+            var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+            config.Filters.Add(new AuthorizeFilter(policy));
+        });
 
         builder.Services.AddControllersWithViews();
 
@@ -20,6 +42,8 @@ public class Program
         app.UseStaticFiles();
 
         app.UseRouting();
+
+        app.UseAuthentication();
 
         app.UseAuthorization();
 
