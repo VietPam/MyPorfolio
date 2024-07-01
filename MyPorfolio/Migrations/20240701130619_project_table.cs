56f7bd93063b2683c9e0dbd4a1c38865141c6af3
phamquangviet891 <82923647+VietPam@users.noreply.github.com>
2024-07-01 20:07:51 +0700

    feat: add project entity

---
+++ MyPorfolio/Migrations/20240701130619_project_table.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyPorfolio.Migrations
+{
+    /// <inheritdoc />
+    public partial class project_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    ProjectID = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ProjectName = table.Column<string>(type: "TEXT", maxLength: 150, nullable: false),
+                    ProjectDescription = table.Column<string>(type: "TEXT", nullable: false),
+                    ProjectDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    ProjectPic = table.Column<string>(type: "TEXT", maxLength: 255, nullable: true),
+                    ProjectInfo = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.ProjectID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
--- MyPorfolio/Models/Context/MyPorfolioContext.cs
+++ MyPorfolio/Models/Context/MyPorfolioContext.cs
@@ -8,6 +8,7 @@ public class MyPorfolioContext :DbContext
     public DbSet<Admin> Admins { get; set; }
     public DbSet<About> Abouts { get; set; }
     public DbSet<Contact> Contacts { get; set; }
+    public DbSet<Project> Projects { get; set; }
 
     public MyPorfolioContext()
     {
